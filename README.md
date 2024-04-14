6d5eff5258f1ecef25e6b89cf3b6d6fb868c3a4c
vaibhavMahmia <vaibhavwwe76@gmail.com>
2024-04-14 22:25:56 +0530

    SUN_APR_14_2024_10_43_PM_IST

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Push & Pull working.
 <h1>Last Left: 04:55:11</h1>
 
 ### SignIn SignUp frontend created ...
+
+# Home Page
+
+![HOME PAGE](./images/homePage.png "HOME PAGE")
--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import {React} from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Container } from "@material-ui/core";
 import Navbar from "./components/Navbar/Navbar";
 import Home from './components/Home/Home';
@@ -7,13 +7,16 @@ import Auth from './components/Auth/Auth'
 
 
 const App = () => {
+  const user = JSON.parse(localStorage.getItem('profile'));
   return (
     <BrowserRouter>
-    <Container maxWidth="lg">
+    <Container maxWidth="xl">
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route exact path="/auth" element={<Auth/>} />
+        <Route exact path="/" element={<Navigate to="/posts" />} />
+        <Route path="/posts" exact element={<Home/>} />
+        <Route path="/posts/search" exact element={<Home/>} />
+        <Route exact path="/auth" element={(!user ? <Auth /> : <Navigate to="/posts" />)} />
       </Routes>
     </Container>
   </BrowserRouter>
